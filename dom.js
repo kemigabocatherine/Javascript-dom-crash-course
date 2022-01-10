0f97ddd2a15c7262af86978735cea942efd36f5e
kemigabocatherine <kemigabocatherine205@gmail.com>
2022-01-10 21:23:50 +0300

    Selectors in javascript

--- dom.js
+++ dom.js
@@ -1,12 +1,23 @@
 //EXAMINE THE DOCUMENT OBJECT //
 //console.dir(document);
-console.log(document.domain);
-console.log(document.URL);
-console.log(document.title);
+//console.log(document.domain);
+//console.log(document.URL);
+//console.log(document.title);
 //document.title = 123; //can change title
-console.log(document.doctype);
-console.log(document.head);
-console.log(document.body);
-console.log(document.all);
-console.log(document.forms); //Brings out any form in the document
-console.log(document.links);
+//console.log(document.doctype);
+//console.log(document.head);
+//console.log(document.body);
+//console.log(document.all);
+//console.log(document.forms); //Brings out any form in the document
+//console.log(document.links);
+
+
+// SELECTORS //
+// GETELEMENTBYID //
+//console.log(document.getElementById('header-title'));
+var headerTitle = document.getElementById('header-title');
+console.log(headerTitle);
+
+//Changing header content
+headerTitle.textContent = 'Hello';
+headerTitle.innerText = 'Goodbye';
