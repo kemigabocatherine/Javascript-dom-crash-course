2e4339f92a5dcec2674f781b9055d77ea3fef2d7
kemigabocatherine <kemigabocatherine205@gmail.com>
2022-01-10 18:14:04 +0300

    Basic thing you can do with javascript dom

---
+++ dom.js
@@ -0,0 +1,12 @@
+//EXAMINE THE DOCUMENT OBJECT //
+//console.dir(document);
+console.log(document.domain);
+console.log(document.URL);
+console.log(document.title);
+//document.title = 123; //can change title
+console.log(document.doctype);
+console.log(document.head);
+console.log(document.body);
+console.log(document.all);
+console.log(document.forms); //Brings out any form in the document
+console.log(document.links);
