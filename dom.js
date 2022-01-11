6fa985db99874c83b01a39c2fc0ac891864581a6
Kemigabo Catherine <kemigabocatherine205@gmail.com>
2022-01-11 17:01:56 +0300

    Using query selector all

--- dom.js
+++ dom.js
@@ -60,19 +60,36 @@ for(var i = 0; i < items.length; i++){
 // }
 
 //QUERY SELECTOR // ->only one item (the first one)
-var header = document.querySelector('#main-header');
-header.style.borderBottom = 'solid 4px #000';
+// var header = document.querySelector('#main-header');
+// header.style.borderBottom = 'solid 4px #000';
 
-var input = document.querySelector('input');
-input.value = 'Hello world';
+// var input = document.querySelector('input');
+// input.value = 'Hello world';
 
-var submit = document.querySelector('input[type= "submit"]');
-submit.value = "SEND";
+// var submit = document.querySelector('input[type= "submit"]');
+// submit.value = "SEND";
 
-var item = document.querySelector('.list-group-item');
-item.style.color = 'red';
+// var item = document.querySelector('.list-group-item');
+// item.style.color = 'red';
 
-var lastItem = document.querySelector('.list-group-item:last-child');
-lastItem.style.color = 'blue';
+// var lastItem = document.querySelector('.list-group-item:last-child');
+// lastItem.style.color = 'blue';
+
+// var secondItem = document.querySelector('.list-group-item:nth-child(2)');
+// secondItem.style.color = 'purple';
+
+//QUERY SELECTOR ALL//
+var titles = document.querySelectorAll('.title');
+
+console.log(titles);
+titles[0].textContent = 'Hello';
+
+var odd = document.querySelectorAll('li:nth-child(odd)');
+var even = document.querySelectorAll('li:nth-child(even)');
+
+for(var i = 0; i < odd.length; i++){
+    odd[i].style.backgroundColor = 'skyBlue';
+    even[i].style.backgroundColor = 'orange';
+}
 
 
